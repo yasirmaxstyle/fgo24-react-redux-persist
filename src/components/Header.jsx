f891cd7ecc6676c5c559d12aca9ff90fe96de12d
yasirmaxstyle <yasirmu77@gmail.com>
2025-05-20 17:10:32 +0700

    fix: edit header tagline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 function Header() {
   return (
     <div>
-      <div className="max-w-md mx-auto h-10 bg-black text-white">
-        To Do List
+      <div className="max-w-md mx-auto py-3 bg-black text-white flex flex-col justify-center px-5">
+        <h1 className="font-bold text-2xl">To Do List App</h1>
+        <p>Get moved, don't stay!!!</p>
       </div>
     </div>
   )
