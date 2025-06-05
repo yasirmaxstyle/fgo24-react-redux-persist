1690da71250eb2c2be2287a5f4e167f11ec1d900
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-05 07:25:22 +0700

    feat: switch usecontext and state with redux as state global management storage

--- src/components/ShowTodos.jsx
+++ src/components/ShowTodos.jsx
@@ -1,44 +1,49 @@
-import { useContext, useState } from 'react'
-import { TaskLists } from './TaskContext'
 import { RiDeleteBinFill } from "react-icons/ri";
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteTask, toggleCheck } from '../redux/reducers/todos';
 
-function ShowTodos({ onDelete }) {
-  const todoList = useContext(TaskLists)
-  const [completed, setCompleted] = useState(0)
+function ShowTodos() {
+  const dispatch = useDispatch()
 
-  const handleChecked = (e) => {
-    if (e.target.checked && e.target.id === e.target.nextSibling.htmlFor) {
+  const todos = useSelector(state => state.todos.data)
+  const completed = todos.filter(e => e.checked === true)
+
+  const handleChecked = (e, id, checked) => {
+    if (checked) {
       e.target.nextSibling.classList.add('line-through')
-      setCompleted(completed + 1)
+      dispatch(toggleCheck({
+        id,
+        checked: true
+      }))
     } else {
       e.target.nextSibling.classList.remove('line-through')
-      setCompleted(completed - 1)
+      dispatch(toggleCheck({
+        id,
+        checked: false
+      }))
     }
   }
 
-  const handleDelete = (e) => {
-    if (e.currentTarget.id === e.currentTarget.previousSibling.id) {
-      onDelete(todoList.filter((_, idx) => idx !== Number(e.currentTarget.id)))
-      setCompleted(completed - 1)
-    }
+  const handleDelete = (id) => {
+    dispatch(deleteTask(id))
   }
 
   return (
     <div className='text-center grid gap-5'>
       <div className='flex flex-col gap-3'>
-        {todoList.map((todo, idx) =>
-          <div key={idx} className='w-full flex gap-3 border px-5 py-3 rounded'>
-            <input type="checkbox" name="todo" id={todo} onChange={handleChecked} />
-            <label id={idx} htmlFor={todo} className='grow text-left'>{todo}</label>
-            <button id={idx} className='hover:cursor-pointer' onClick={handleDelete}>
+        {todos.map((todo) =>
+          <div key={`list-${todo.id}`} className='w-full flex gap-3 border px-5 py-3 rounded'>
+            <input type="checkbox" name="todo" id={`list-${todo.id}`} onChange={(e) => handleChecked(e, todo.id, e.target.checked)} />
+            <label htmlFor={`list-${todo.id}`} className='grow text-left'>{todo.taskname}</label>
+            <button className='hover:cursor-pointer' onClick={() => handleDelete(todo.id)}>
               <RiDeleteBinFill className='text-2xl' />
             </button>
           </div>
         )}
       </div>
       <div>
-        <p>{completed > 0 && `You have completed ${completed} tasks`}</p>
-        <p>{completed > 0 && completed === todoList.length && `Congratulations, you have done all tasks!!!`}</p>
+        <p>{completed.length > 0 && `You have completed ${completed.length} tasks`}</p>
+        <p>{completed.length > 0 && completed.length === todos.length && `Congratulations, you have done all tasks!!!`}</p>
       </div>
     </div>
   )
