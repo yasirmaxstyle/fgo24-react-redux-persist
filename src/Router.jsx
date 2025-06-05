52c4dc6a41b0b3e5974f4ac6e5db9a57892564f2
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-05 07:27:04 +0700

    feat: add redux provider for wrapping the route to pass the global state within

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,9 @@
 import { Outlet, Route, Routes } from "react-router"
 import Header from "./components/Header"
 import HomePage from "./pages/HomePage"
+import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
+import { persistor, store } from "./redux/store"
 
 function Layout() {
   return (
@@ -13,11 +16,15 @@ function Layout() {
 
 function Router() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<HomePage />} />
-      </Route>
-    </Routes>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+      </PersistGate>
+    </Provider>
   )
 }
 
