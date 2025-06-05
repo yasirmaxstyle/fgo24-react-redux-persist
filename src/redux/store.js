481474ace92dc6ab1e607b1cbb7483fab52bb5a6
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-05 07:29:22 +0700

    feat: add redux store to for state storage

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore } from "redux-persist";
+import reducer from "./reducers";
+
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"]
+      }
+    })
+})
+
+export const persistor = persistStore(store)
